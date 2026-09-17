11169e96d50fc422f0118661ff927d03fee3395d
hamza-code-hub <maherhamza4616@gmail.com>
2026-09-18 04:36:41 +0500

    Add Publication

--- js/sidebar.js
+++ js/sidebar.js
@@ -38,7 +38,7 @@
           '<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>' +
           '<div id="navigation" class="collapse navbar-collapse flex-column">' +
             '<div class="profile-section pt-3 pt-lg-0">' +
-              '<img class="profile-image sidebar-profile-image mb-3 rounded-circle mx-auto" src="' + base + 'images/hamza%20gray%20suite.png" alt="Muhammad Hamza AI Developer in a gray suit" width="160" height="160" loading="eager">' +
+              '<img class="profile-image sidebar-profile-image mb-3 rounded-circle mx-auto" src="' + base + 'images/hamza_gray_professional.png" alt="Muhammad Hamza AI Developer professional portrait" width="160" height="160" loading="eager">' +
               '<div class="bio mb-3">Hi, I am <strong>Muhammad Hamza.</strong></div>' +
               '<ul class="social-list list-inline py-2 mx-auto">' +
                 '<li class="list-inline-item"><a href="https://twitter.com/Muhamma08974837" target="_blank" rel="noopener noreferrer"><i class="fab fa-twitter fa-fw"></i></a></li>' +
